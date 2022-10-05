74f73148ee060c0f8d6e1b58cd8f06d3275a3a99
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-10-05 15:25:28 -0500

    Control flow exercise

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,15 @@
+
+public class ControlFlowExercises {
+
+    public static void main(String[] args) {
+        //Loop Basic
+        // while loop
+        int whileNum = 5;
+        while (whileNum <= 15 ){
+            System.out.println(whileNum);
+            whileNum++;
+        }
+
+
+    }
+}
