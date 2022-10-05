f7d86aaf417baee448a212292672ff4ca896e3b2
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-10-05 10:13:40 -0500

    SyntaxTypesAndVariables exercise

--- src/SyntaxTypesAndVariables.java
+++ src/SyntaxTypesAndVariables.java
@@ -6,15 +6,22 @@ public class SyntaxTypesAndVariables {
         char newString;
         newString = 'Y';
         double string = 3.14159;
-        long myNumber;
+//        long myNumber;
 //        myNumber = 3.14;
 //        myNumber = 123L;
-        myNumber = 123;
+//        myNumber = 123;
+        float myNumber;
+        myNumber = (float) 3.14;
+        double myNewNumber = myNumber;
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
         System.out.println("Favorite int number is " + myFavoriteNumber);
         System.out.println("Name as a string " + myString);
         System.out.println(newString);
         System.out.println(string);
         System.out.println(myNumber);
+        System.out.println(myNewNumber);
 
 
     }
