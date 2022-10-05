feed489507855bdd8f321f148d720e188ccb50d2
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-10-05 09:36:52 -0500

    SyntaxTypesAndVariables exercise

---
+++ src/SyntaxTypesAndVariables.java
@@ -0,0 +1,22 @@
+public class SyntaxTypesAndVariables {
+
+    public static void main(String[] args) {
+        int myFavoriteNumber = 619;
+        String myString = "Yogesh";
+        char newString;
+        newString = 'Y';
+        double string = 3.14159;
+        long myNumber;
+//        myNumber = 3.14;
+//        myNumber = 123L;
+        myNumber = 123;
+        System.out.println("Favorite int number is " + myFavoriteNumber);
+        System.out.println("Name as a string " + myString);
+        System.out.println(newString);
+        System.out.println(string);
+        System.out.println(myNumber);
+
+
+    }
+
+}
