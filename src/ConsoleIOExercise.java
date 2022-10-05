828e33ec05b663d5b8da8d30b8f98620952f4031
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-10-05 12:21:57 -0500

    retrieving each words from the variables

--- src/ConsoleIOExercise.java
+++ src/ConsoleIOExercise.java
@@ -5,6 +5,20 @@ public class ConsoleIOExercise {
     public static void main(String[] args) {
         double pi = 3.14159;
         System.out.printf("The value of pi is approximately %.2f.%n",pi);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Enter an integer: %n");
+        int nxtInt = scanner.nextInt();
+
+        System.out.printf("Enter 3 words: %n");
+        String firstWord = scanner.next();
+        String secondWord = scanner.next();
+        String thirdWord = scanner.next();
+        System.out.printf("The first word you entered is: %s %n", firstWord);
+        System.out.printf("The second word you entered is: %s %n", secondWord);
+        System.out.printf("The third word you entered is: %s %n", thirdWord);
+
+
     }
 
 }
