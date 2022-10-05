44f9666a999993dd6e2464422bc6e10c4eeaa6c2
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-10-05 12:12:12 -0500

    Console IO exercise

--- src/ConsoleIOExercise.java
+++ src/ConsoleIOExercise.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class ConsoleIOExercise {
 
     public static void main(String[] args) {
-
+        double pi = 3.14159;
+        System.out.printf("The value of pi is approximately %.2f.%n",pi);
     }
 
 }
