7a9b4f4b37ed6f656817c29b4eed8e954e001882
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-10-06 15:33:49 -0500

    created addition method, takes two parameters.

--- src/MethodExercise.java
+++ src/MethodExercise.java
@@ -1,2 +1,17 @@
+
 public class MethodExercise {
+    public static void main(String[] args) {
+
+        addition(5, 7);
+
+
+    }
+
+    //Addition taking two parameters.
+    public static void addition(int input1, int input2){
+        int additionInput = input1 + input2;
+        System.out.printf("Your result for the addition is: %d", additionInput);
+    }
+
+
 }
