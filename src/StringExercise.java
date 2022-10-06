aac74303ad867836c57340c43c9c8a5a861a5767
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-10-06 09:12:09 -0500

    creating a string variable and printing it in the console

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -6,8 +6,12 @@ public class StringExercise {
         //creating a string variable and printing it in the console.
         String string = "We don't need no education \nWe don't need no thought control";
         System.out.println(string);
+
         String string1 = "Check \"this\" out!, \"s inside of \"s!";
         System.out.println(string1);
+
+        String string2 = "In windows, the main drive is usually c:\\";
+        System.out.println(string2);
 
 
     }
