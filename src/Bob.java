0e416de655f5282e39f46f131dac860edf732779
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-10-06 10:22:33 -0500

    conversation with limited auto response.

---
+++ src/Bob.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Bob {
+    public static void main(String[] args) {
+        boolean conversation = false;
+        do {
+            System.out.println("Ask question to Bob:");
+            Scanner scanner = new Scanner(System.in);
+            String userInput = scanner.nextLine();
+
+            if (userInput.endsWith("?")){
+                System.out.println("Sure.");
+            }
+            else if (userInput.endsWith("!")){
+                System.out.println("Whoa, chill out!");
+            }
+            else if (userInput.endsWith("")) {
+                System.out.println("Fine. Be that way!");
+            }
+            else {
+                System.out.println("whatever");
+            }
+
+        }while (conversation == true);
+
+
+    }
+}
