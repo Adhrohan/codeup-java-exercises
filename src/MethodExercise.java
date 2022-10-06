0f2a03fff171f301e5c2e0cd74e480b0cbe7ae2b
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-10-06 15:38:10 -0500

    created subtraction method, takes two parameters.

--- src/MethodExercise.java
+++ src/MethodExercise.java
@@ -3,6 +3,7 @@ public class MethodExercise {
     public static void main(String[] args) {
 
         addition(5, 7);
+        subtraction(7,5);
 
 
     }
@@ -10,7 +11,13 @@ public class MethodExercise {
     //Addition taking two parameters.
     public static void addition(int input1, int input2){
         int additionInput = input1 + input2;
-        System.out.printf("Your result for the addition is: %d", additionInput);
+        System.out.printf("Your result for the addition is: %d%n", additionInput);
+    }
+
+    //Subtraction taking two parameter as an input.
+    public static void subtraction(int input1, int input2){
+        int subtraction = input1 - input2;
+        System.out.printf("Your result for the subtraction is: %d %n", subtraction);
     }
 
 
