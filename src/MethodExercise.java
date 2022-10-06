6a22f02f861140e7e7f4fba7660ec35df613d7ba
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-10-06 15:43:55 -0500

    created division method, takes two parameters.

--- src/MethodExercise.java
+++ src/MethodExercise.java
@@ -5,26 +5,33 @@ public class MethodExercise {
         addition(5, 7);
         subtraction(7,5);
         multiplication(5,7);
+        division(10,5);
 
 
     }
 
-    //Addition taking two parameters.
+    //Addition method taking two parameters.
     public static void addition(int input1, int input2){
         int additionInput = input1 + input2;
         System.out.printf("Your result for the addition is: %d%n", additionInput);
     }
 
-    //Subtraction taking two parameter as an input.
+    //Subtraction method taking two parameter as an input.
     public static void subtraction(int input1, int input2){
         int subtraction = input1 - input2;
         System.out.printf("Your result for the subtraction is: %d %n", subtraction);
     }
 
-    //Multiplication taking two parameters.
+    //Multiplication method taking two parameters.
     public static void multiplication(int input1, int input2){
         int multiplication = input1 * input2;
-        System.out.printf("Your result for the multiplication is: %d", multiplication);
+        System.out.printf("Your result for the multiplication is: %d %n", multiplication);
+    }
+
+    //Division method taking two parameters.
+    public static void division(int input1, int input2){
+        int division = input1 / input2;
+        System.out.printf("Your result for the division is:%d %n", division);
     }
 
 
