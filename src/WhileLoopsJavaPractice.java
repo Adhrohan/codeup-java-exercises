a798961d9e50812d7de1348b5018303b72a13764
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-10-07 14:31:36 -0500

    java while/ do-while loop practice

---
+++ src/WhileLoopsJavaPractice.java
@@ -0,0 +1,16 @@
+
+public class WhileLoopsJavaPractice {
+    public static void main(String[] args) {
+        int a = 0;
+
+        do {
+            System.out.println(a);
+        }while (a==1 );
+//        while (a < 10){
+//            System.out.printf("the value of a is less than 10: %d%n",a);
+//            a++;
+//        }
+
+
+    }
+}
