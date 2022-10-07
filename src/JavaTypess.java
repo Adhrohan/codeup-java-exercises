b506c6d7640e4c5b0bb35870b0f6ed469c6f3b18
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-10-07 11:58:26 -0500

    java types practice

---
+++ src/JavaTypess.java
@@ -0,0 +1,25 @@
+
+public class JavaTypess {
+    public static void main(String[] args) {
+        int a = 9;
+        long b = 999999999999l;
+        float c = 5.54f;
+        double d = 5.454;
+        String e = "hello, Yogesh!";
+        byte f =  127;
+        boolean g = true;
+        char i = 'H';
+        short j = 28776;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(i);
+        System.out.println(j);
+    }
+
+
+}
