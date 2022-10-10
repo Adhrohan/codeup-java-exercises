90ac3d9ef61fca2155cd2b0576d7e5533cf17c83
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-10-09 22:36:35 -0500

    java method exercise

--- src/MethodExercise.java
+++ src/MethodExercise.java
@@ -1,51 +1,54 @@
+
 import java.util.Scanner;
 import java.lang.Math;
 public class MethodExercise {
     public static void main(String[] args) {
 
         addition(5, 7);
-        subtraction(7,5);
-        multiplication(5,7);
-        division(10,5);
+        subtraction(7, 5);
+        multiplication(5, 7);
+        division(10, 5);
         modulus(7, 5);
 //        getInteger(1,10);
 //        System.out.println(getFactorial(5));
 
 
-
-
-
     }
 
     //Addition method taking two parameters.
-    public static void addition(int input1, int input2){
+    public static void addition(int input1, int input2) {
         int additionInput = input1 + input2;
         System.out.printf("Your result for the addition is: %d%n", additionInput);
-    };
+    }
+
 
     //Subtraction method taking two parameter as an input.
-    public static void subtraction(int input1, int input2){
+    public static void subtraction(int input1, int input2) {
         int subtraction = input1 - input2;
         System.out.printf("Your result for the subtraction is: %d %n", subtraction);
-    };
+    }
+
 
     //Multiplication method taking two parameters.
-    public static void multiplication(int input1, int input2){
+    public static void multiplication(int input1, int input2) {
         int multiplication = input1 * input2;
         System.out.printf("Your result for the multiplication is: %d %n", multiplication);
-    };
+    }
+
 
     //Division method taking two parameters.
-    public static void division(int input1, int input2){
+    public static void division(int input1, int input2) {
         int division = input1 / input2;
         System.out.printf("Your result for the division is:%d %n", division);
-    };
+    }
+
 
     //Modulus method taking two parameters.
-    public static void modulus(int input1, int input2){
+    public static void modulus(int input1, int input2) {
         int modulus = input1 % input2;
         System.out.printf("Your remainder for the numbers provided is:%d %n", modulus);
-    };
+    }
+
 
     //Method to validate user input is in given range.
 //    public static int getInteger(int min, int max){
@@ -75,7 +78,11 @@ public class MethodExercise {
 //
 //        }
 //        return userInput;
-    }
+
+
+
+}
+
 
 
 
