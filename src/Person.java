b7381f09fddb86a503caffa0b6801730b7c3bbf0
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-10-11 11:57:42 -0500

    OOP exercise

--- src/Person.java
+++ src/Person.java
@@ -2,36 +2,48 @@
 
 // defining a person class and creating an instance of it.
  class Person {
-    public String firstName;
-    public String lastName;
+    public static void main(String[] args) {
 
-    public String sayHello(){
-        return String.format("Hello from %s %s!", firstName, lastName);
-     }
+        Person yogesh = new Person();
+        yogesh.name = "Yogesh";
+        System.out.println(yogesh.getName());
+    }
+    public String name;
+    public String getName(){
+        return String.format(name);
+    }
 
-     public static void main(String[] args) {
-         Person rick = new Person();
-         rick.firstName = "Rick";
-         rick.lastName = "Sanchez";
-         System.out.println(rick.sayHello());
 
-         Person ada = new Person();
-         ada.firstName = "Ada";
-         ada.lastName = "Lovelace";
-         System.out.println(ada.sayHello());
-
-         System.out.println(Math.PI);
-
-         Person theBestDrummerAlive = new Person();
-         theBestDrummerAlive.name = "Neil Peart";
-
-         Person.worldPopulation += 1;
-         System.out.println(Person.worldPopulation);
-         System.out.println(theBestDrummerAlive.worldPopulation);
-     }
-
-     public static long worldPopulation = 750000000L;
-     public String name;
+//    public String firstName;
+//    public String lastName;
+//
+//    public String sayHello(){
+//        return String.format("Hello from %s %s!", firstName, lastName);
+//     }
+//
+//     public static void main(String[] args) {
+//         Person rick = new Person();
+//         rick.firstName = "Rick";
+//         rick.lastName = "Sanchez";
+//         System.out.println(rick.sayHello());
+//
+//         Person ada = new Person();
+//         ada.firstName = "Ada";
+//         ada.lastName = "Lovelace";
+//         System.out.println(ada.sayHello());
+//
+//         System.out.println(Math.PI);
+//
+//         Person theBestDrummerAlive = new Person();
+//         theBestDrummerAlive.name = "Neil Peart";
+//
+//         Person.worldPopulation += 1;
+//         System.out.println(Person.worldPopulation);
+//         System.out.println(theBestDrummerAlive.worldPopulation);
+//     }
+//
+//     public static long worldPopulation = 750000000L;
+//     public String name;
 
 
 }
