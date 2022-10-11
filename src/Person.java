7335cec629c740809fcb36cb770343d8c3a27b1f
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-10-11 10:50:54 -0500

    Defining a person class and creating an instance of it.

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,18 @@
 
 
-public class Person {
+// defining a person class and creating an instance of it.
+ class Person {
+    public String firstName;
+    public String lastName;
+
+    public String sayHello(){
+        return String.format("Hello from %s %s!", firstName, lastName);
+     }
+
+     public static void main(String[] args) {
+         Person rick = new Person();
+         rick.firstName = "Rick";
+         rick.lastName = "Sanchez";
+         System.out.println(rick.sayHello());
+     }
 }
