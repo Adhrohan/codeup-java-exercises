1f58313a98c586807a31bbd7c49dfa60dd47f7b6
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-10-12 09:49:07 -0500

    java arrays practice

---
+++ src/ArraysPractice.java
@@ -0,0 +1,13 @@
+public class ArraysPractice {
+    public static void main(String[] args) {
+        // using the array initializer syntax
+        String[] beatles = {"John", "Paul", "Ringo"};
+        System.out.println(beatles.length);
+        System.out.println(beatles[0]); // "John"
+        System.out.println(beatles[1]); // "Paul"
+        System.out.println(beatles[2]); // "Ringo"
+        //System.out.println(beatles[3]); // Error Index 3 out of bounds for length 3
+
+        // Iterating through array using for loop.
+    }
+}
