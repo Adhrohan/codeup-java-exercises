bbb2aa68df49c814bd5ee6ecfcf3755be0db4446
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-10-28 10:49:48 -0500

    serrver name generator exercise completed

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,7 +1,21 @@
+import java.util.Random;
 
 public class ServerNameGenerator {
+
+    public static void main(String[] args) {
+        String random1 = ServerNameGenerator.randomElement(adjectives);
+        String random2 = ServerNameGenerator.randomElement(nouns);
+
+        System.out.println("Yur server name is "+ "'"+random1+ " " + random2 + "'.");
+    }
+
     public static String[] adjectives = {"fast", "slow", "quick", "average", "important","lazy","great","spectacular","bitter","classic" };
     public static String[] nouns = {"horse", "cat", "dog", "duck", "elephant","mountain","tesla","toyota","gmc","lamborghini"};
+
+    public static String randomElement(String [] arr){
+        int randomInt = new Random().nextInt(arr.length);
+        return arr[randomInt];
+    }
 
 
 }
