fd83ae03a0d5ed57c35a48ec1e0b6cb4a87b4d17
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-10-29 13:47:20 -0500

    collection exercises

--- src/javaPractice/review.java
+++ src/javaPractice/review.java
@@ -1,51 +1,51 @@
-package javaPractice;
-
-public class review {
-    public static void main(String[] args) {
-        System.out.println(circumference(5));
-        System.out.println(circumference1(5));
-        System.out.println(total(100,10));
-        System.out.println(numEven() = {1,2,3,4,5,6,7,8,9};);
-
-
-    }
-
-
-
-    public static double circumference(double r){
-        return 2 * Math.PI * r;
-    }
-
-    public static int circumference1(int r){
-
-         double circumference1 = (2 * Math.PI * r);
-         return (int) circumference1;
-    }
-
-    public static double total(double bill, double tipPercentage){
-        return bill + ((tipPercentage/100)*bill);
-    }
-
-//    public static void even(int[] numbers){
-//        if (numbers % 2 == 0){
-//            return numbers;
-//        }
+//package javaPractice;
+//
+//public class review {
+//    public static void main(String[] args) {
+//        System.out.println(circumference(5));
+//        System.out.println(circumference1(5));
+//        System.out.println(total(100,10));
+//        System.out.println(numEven() = {1,2,3,4,5,6,7,8,9};);
+//
+//
 //    }
-
-    private int[] numbers;
-    public Scores(int[] numbersIn) {
-        numbers = numbersIn;
-    }
-    public static int numEven() {
-        int evens = 0;
-        for (int i = 0; i < list.length; i++)
-            if (list[i] % 2 == 0)
-                evens++;
-
-        return evens;
-    }
-
-
-
-
-}
+//
+//
+//
+//    public static double circumference(double r){
+//        return 2 * Math.PI * r;
+//    }
+//
+//    public static int circumference1(int r){
+//
+//         double circumference1 = (2 * Math.PI * r);
+//         return (int) circumference1;
+//    }
+//
+//    public static double total(double bill, double tipPercentage){
+//        return bill + ((tipPercentage/100)*bill);
+//    }
+//
+////    public static void even(int[] numbers){
+////        if (numbers % 2 == 0){
+////            return numbers;
+////        }
+////    }
+//
+//    private int[] numbers;
+//    public Scores(int[] numbersIn) {
+//        numbers = numbersIn;
+//    }
+//    public static int numEven() {
+//        int evens = 0;
+//        for (int i = 0; i < list.length; i++)
+//            if (list[i] % 2 == 0)
+//                evens++;
+//
+//        return evens;
+//    }
+//
+//
+//
+//
+//}
